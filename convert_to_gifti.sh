e832387c833fb840f4d071a1bdbf6137c297df49
Kelly Martin <kccmartin492@gmail.com>
2017-11-27 13:57:47 -0500

    Add files via upload

---
+++ convert_to_gifti.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+export maindir=/Users/newportlab/Desktop
+export subdir=$maindir/subjects_postrecon2
+export subs=`ls $subdir`
+
+for subject in $subs
+do
+#mkdir $maindir/subjects/$subject
+#cp $subdir/$subject/SPM/s*.nii $maindir/subjects/$subject
+
+cd $subdir/$subject/surf
+
+mris_convert lh.white lh.white.gii
+mris_convert lh.pial lh.pial.gii
+mris_convert rh.white rh.white.gii
+mris_convert rh.pial rh.pial.gii
+
+done
+
+echo ALL done
+
+
---
+++ copyfiles.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+export maindir=/Users/newportlab/Desktop
+# export subdir=$maindir/HealthyKidRSFC
+export subdir=$maindir/HealthyAdultRSFC
+export subs=`ls $subdir`
+
+for subject in $subs
+do
+mkdir $maindir/subjects/$subject
+cp $subdir/$subject/SPM/s*.nii $maindir/subjects/$subject
+done
+
+echo ALL done
+
+
